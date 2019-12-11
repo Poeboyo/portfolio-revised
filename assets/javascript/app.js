4f34d5f63c84db05b2df8109043849e9bfd455dd
Jeffrey Poe <jeffreypoe@jeffreys-mbp-2.lan>
2019-12-11 17:55:51 -0500

    added files

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(".button-collapse").sideNav();
+});
+$(document).ready(function() {
+  $(".parallax").parallax();
+});
+$(".carousel.carousel-slider").carousel({
+  fullWidth: false
+});
+$(document).ready(function() {
+  $(".carousel").carousel();
+});
+AOS.init();
